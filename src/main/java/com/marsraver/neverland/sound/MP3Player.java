00fb61a033f4b05031077ab10bfe9da4182e15e3
jwindberg <john@windberg.name>
2023-09-18 12:37:56 -0700

    sound player now multithreaded

--- src/main/java/com/marsraver/neverland/sound/MP3Player.java
+++ src/main/java/com/marsraver/neverland/sound/MP3Player.java
@@ -1,29 +1,44 @@
 package com.marsraver.neverland.sound;
 
-import java.io.InputStream;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Component
 public class MP3Player {
 
-  public void play(Sound sound) {
-    play(this.getClass().getResourceAsStream(sound.file));
-  }
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-
-  public void play(String classPathResource) {
-    play(this.getClass().getResourceAsStream(classPathResource));
-  }
-
-  public void play(InputStream inputStream) {
-    try {
-      Player player = new Player(inputStream);
-      player.play();
-    } catch (JavaLayerException e) {
-      throw new RuntimeException(e);
+    public void play(Sound sound) {
+        play(this.getClass().getResourceAsStream(sound.file));
     }
-  }
+
+    public void play(String classPathResource) {
+        play(this.getClass().getResourceAsStream(classPathResource));
+    }
+
+    public void play(InputStream inputStream) {
+        executorService.submit(new SoundCall(inputStream));
+    }
+
+    @AllArgsConstructor
+    private class SoundCall implements Runnable {
+        private InputStream inputStream;
+
+        @Override
+        public void run() {
+            try {
+                Player player = new Player(inputStream);
+                player.play();
+            } catch (JavaLayerException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
 }
